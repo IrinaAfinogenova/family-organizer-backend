80f31f3508d2ba72a5b018d43d0afcf9657c033b
IrinaAfinogenova <afinogenovair1996@gmail.com>
2025-08-29 13:35:13 +0200

    cors testing fix

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173", // TODO update to real place
+  origin: true, // TODO fix it when app will be public
 }));
 app.use(express.json());
 
